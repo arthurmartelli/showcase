56745f8a12345774c8192036f41fc9b2b3ca57f4
Arthur Martelli <arthur.martellig@gmail.com>
2024-04-05 14:21:43 -0600

    added writer to VGA buffer

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -3,29 +3,20 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // function called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // ! don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
 
-    // write the string `Hello World` to the screen through the VGA text buffer
-    // the VGA text buffer is assumed to start at address 0xb8000 in memory
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
