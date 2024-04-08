73ee6f87d52efd81498b41c1498ec81e45d2b81a
Arthur Martelli <arthur.martellig@gmail.com>
2024-04-08 12:27:29 -0600

    update

--- rust_os/src/vga_buffer/mod.rs
+++ rust_os/src/vga_buffer/mod.rs
@@ -1,3 +1,7 @@
+use core::fmt;
+
+use volatile::Volatile;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -42,7 +46,7 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
@@ -66,10 +70,12 @@ impl Writer {
         let col = self.column_position;
 
         let color_code = self.color_code;
-        self.buffer.chars[row][col] = ScreenChar {
+
+        self.buffer.chars[row][col].write(ScreenChar {
             ascii_character: byte,
             color_code,
-        };
+        });
+
         self.column_position += 1;
     }
 
@@ -87,8 +93,8 @@ impl Writer {
     fn new_line(&mut self) {
         for row in 1..BUFFER_HEIGHT {
             for col in 0..BUFFER_WIDTH {
-                let character = self.buffer.chars[row][col];
-                self.buffer.chars[row - 1][col] = character;
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
             }
         }
 
@@ -103,8 +109,15 @@ impl Writer {
         };
 
         for col in 0..BUFFER_WIDTH {
-            self.buffer.chars[row][col] = blank;
+            self.buffer.chars[row][col].write(blank);
         }
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
     }
 }
 
